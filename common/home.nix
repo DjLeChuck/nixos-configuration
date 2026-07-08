1b22d353bdd13b03ed3edde713d74c5c63304371
DjLeChuck <djlechuck@gmail.com>
2026-07-08 11:03:40 +0200

    fix: NIXOS_CONFIG_DIR env var should be defined first

--- common/home.nix
+++ common/home.nix
@@ -353,48 +353,55 @@ in
   programs.fish = {
     enable = true;
 
-    interactiveShellInit = ''
-      # Remove greeting message
-      set -g fish_greeting
+    interactiveShellInit = pkgs.lib.mkMerge [
+      (pkgs.lib.mkBefore ''
+        # NIXOS_CONFIG_DIR must be set before any other interactiveShellInit
+        # runs, since machine-specific snippets (e.g. NH_OS_FLAKE) expand it
+        # immediately - mkBefore pins this ahead regardless of module order.
+        set -gx NIXOS_CONFIG_DIR ~/.config/nixos-config
+        set -gx NH_FLAKE $NIXOS_CONFIG_DIR
+      '')
+      ''
+        # Remove greeting message
+        set -g fish_greeting
 
-      # Global variables
-      set -gx EDITOR vim
-      set -gx CDG_DIR ~/development/php
-      set -gx GPG_TTY (tty)
-      set -gx NIXOS_CONFIG_DIR ~/.config/nixos-config
-      set -gx NH_FLAKE $NIXOS_CONFIG_DIR
+        # Global variables
+        set -gx EDITOR vim
+        set -gx CDG_DIR ~/development/php
+        set -gx GPG_TTY (tty)
 
-      # Colors (custom theme)
-      set -g fish_color_autosuggestion 969896
-      set -g fish_color_cancel -r
-      set -g fish_color_command c397d8
-      set -g fish_color_comment e7c547
-      set -g fish_color_cwd green
-      set -g fish_color_cwd_root red
-      set -g fish_color_end c397d8
-      set -g fish_color_error d54e53
-      set -g fish_color_escape 00a6b2
-      set -g fish_color_history_current --bold
-      set -g fish_color_host normal
-      set -g fish_color_host_remote yellow
-      set -g fish_color_match --background=brblue
-      set -g fish_color_normal normal
-      set -g fish_color_operator 00a6b2
-      set -g fish_color_param 7aa6da
-      set -g fish_color_quote b9ca4a
-      set -g fish_color_redirection 70c0b1
-      set -g fish_color_search_match white --background=brblack
-      set -g fish_color_selection white --bold --background=brblack
-      set -g fish_color_status red
-      set -g fish_color_user brgreen
-      set -g fish_color_valid_path --underline
-      set -g fish_key_bindings fish_default_key_bindings
-      set -g fish_pager_color_completion normal
-      set -g fish_pager_color_description B3A06D yellow
-      set -g fish_pager_color_prefix normal --bold --underline
-      set -g fish_pager_color_progress brwhite --background=cyan
-      set -g fish_pager_color_selected_background -r
-    '';
+        # Colors (custom theme)
+        set -g fish_color_autosuggestion 969896
+        set -g fish_color_cancel -r
+        set -g fish_color_command c397d8
+        set -g fish_color_comment e7c547
+        set -g fish_color_cwd green
+        set -g fish_color_cwd_root red
+        set -g fish_color_end c397d8
+        set -g fish_color_error d54e53
+        set -g fish_color_escape 00a6b2
+        set -g fish_color_history_current --bold
+        set -g fish_color_host normal
+        set -g fish_color_host_remote yellow
+        set -g fish_color_match --background=brblue
+        set -g fish_color_normal normal
+        set -g fish_color_operator 00a6b2
+        set -g fish_color_param 7aa6da
+        set -g fish_color_quote b9ca4a
+        set -g fish_color_redirection 70c0b1
+        set -g fish_color_search_match white --background=brblack
+        set -g fish_color_selection white --bold --background=brblack
+        set -g fish_color_status red
+        set -g fish_color_user brgreen
+        set -g fish_color_valid_path --underline
+        set -g fish_key_bindings fish_default_key_bindings
+        set -g fish_pager_color_completion normal
+        set -g fish_pager_color_description B3A06D yellow
+        set -g fish_pager_color_prefix normal --bold --underline
+        set -g fish_pager_color_progress brwhite --background=cyan
+        set -g fish_pager_color_selected_background -r
+      ''
+    ];
 
     shellAbbrs = {
       dc = "docker compose";
